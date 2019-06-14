7ef1bd2b27e63aa055d5749346949b03846044c9
Sicilat <sicilat@gmail.com>
2019-06-14 03:21:23 +0200

    Pickle's functions added

--- server.py
+++ server.py
@@ -4,6 +4,42 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+import pickle
+
+def unpickle_data(tdata):
+    """
+    Unpickle the data from the client
+    >>> unpickle_data(b'\x80\x03X\n\x00\x00\x00Easy testsq\x00.')
+    "Easy tests"
+    >>> unpickle_data("Easy tests")
+    "Easy tests"
+    """
+    try:
+        if isinstance(tdata, bytes): #Quick check if tdata is already bytes
+            data = pickle.loads(tdata)
+        else:
+            data = tdata
+    except:
+        data = False
+    return data
+
+def pickle_data(tdata):
+    """
+    Pickle the data for the client
+    >>> pickle_data(b'\x80\x03X\n\x00\x00\x00Easy testsq\x00.')
+    b'\x80\x03X\n\x00\x00\x00Easy testsq\x00.'
+    >>> pickle_data("Easy tests")
+    b'\x80\x03X\n\x00\x00\x00Easy testsq\x00.'
+    """
+    try:
+        if isinstance(tdata, bytes): #Quick check if tdata is already bytes
+            data = tdata
+        else:
+            data = pickle.dumps(tdata)
+    except:
+        data = False
+    return data
+
 ###Starting the logger###
 try:
     logging.basicConfig(level=logging.DEBUG)

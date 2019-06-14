1b04328021ce77321392df9613e7ba079fa518a5
Sicilat <sicilat@gmail.com>
2019-06-14 16:18:41 +0200

    Simple multi-threaded server implementation

--- server.py
+++ server.py
@@ -5,6 +5,38 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 import pickle
+import socket
+import threading
+
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def listen(self):
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        global logger
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    #Received data
+                    logger.debug("Received data from client")
+                else:
+                    raise Exception(logger.error("Client disconnected"))
+            except:
+                client.close()
+                return False
 
 def unpickle_data(tdata):
     """
